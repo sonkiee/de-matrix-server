6ed360af64dd616bab415e0bd6de36616093659e
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-18 22:56:20 +0000

    Feb 18, 2025, 11:56 PM

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,19 +1,30 @@
 import express, { Response } from "express";
 import { AuthRequest } from "../middleware/auth.middleware";
-
+import axios from "axios";
 export const initializePayment = async (req: AuthRequest, res: Response) => {
   const user = req.user;
   const { amount, currency, payment_method, description } = req.body;
-  const options = {
-    hostname: "api.paystack.co",
-    port: 443,
-    path: "/transaction/initialize",
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-      "Content-Type": "application/json",
-    },
-  };
+
   try {
-  } catch (error) {}
+    const response = await axios.post(
+      "https://api.paystack.co/transaction/initialize",
+      {
+        amount,
+        currency,
+        payment_method,
+        description,
+        email: user.email,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.status(200).json({ data: response });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+    return;
+  }
 };
