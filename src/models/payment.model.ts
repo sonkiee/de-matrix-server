6f73aae17de908a9d18cadeff48b35d021ea3ce1
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-18 22:38:59 +0000

    Feb 18, 2025, 11:38 PM

---
+++ src/models/payment.model.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const PaymentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    paymentID: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
