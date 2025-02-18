a3e95977e8bf5313a478e9f87a7f78754b078e48
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-18 22:11:18 +0000

    Feb 18, 2025, 11:11 PM

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,49 +1,58 @@
 import mongoose from "mongoose";
 import User from "./user.model";
 
-const OrderSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: User,
-    required: true,
-  },
-  products: [
-    {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-        default: 1,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
+const OrderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User,
+      required: true,
     },
-  ],
-  totalAmount: {
-    type: Number,
-    required: true,
-    default: 0,
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    shippingAddress: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Processing", "Shipped", "Delivered", "cancelled"],
+      default: "Pending",
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["Pending", "Success", "Failed"],
+      default: "Pending",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  shippingAddress: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["Pending", "Processing", "Shipped", "Delivered", "cancelled"],
-    default: "Pending",
-  },
-  paymentStatus: {
-    type: String,
-    enum: ["Pending", "Success", "Failed"],
-    default: "Pending",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.models.Order || mongoose.model("Order", OrderSchema);
