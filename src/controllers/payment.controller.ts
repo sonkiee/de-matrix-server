bb5b4214288a2090adbeee2a5af748f59481659d
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-18 22:51:10 +0000

    Feb 18, 2025, 11:51 PM

---
+++ src/controllers/payment.controller.ts
@@ -0,0 +1,7 @@
+import express, { Response } from "express";
+import { AuthRequest } from "../middleware/auth.middleware";
+
+export const initializePayment = async (req: AuthRequest, res: Response) => {
+  try {
+  } catch (error) {}
+};
