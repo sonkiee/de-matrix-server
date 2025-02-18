a2d2c087f380e7276ba606e9e6a49c3f997b4db1
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-18 22:53:45 +0000

    Feb 18, 2025, 11:53 PM

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -4,7 +4,16 @@ import { AuthRequest } from "../middleware/auth.middleware";
 export const initializePayment = async (req: AuthRequest, res: Response) => {
   const user = req.user;
   const { amount, currency, payment_method, description } = req.body;
-  const options = {};
+  const options = {
+    hostname: "api.paystack.co",
+    port: 443,
+    path: "/transaction/initialize",
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+      "Content-Type": "application/json",
+    },
+  };
   try {
   } catch (error) {}
 };
