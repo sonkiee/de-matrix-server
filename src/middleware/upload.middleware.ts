2390b37c639679166858d18b273601a412530e44
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-20 22:57:53 +0000

    Feb 20, 2025, 11:57 PM

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -6,3 +6,10 @@ export const upload = multer({
     fileSize: 5 * 1024 * 1024,
   },
 });
+
+export const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
