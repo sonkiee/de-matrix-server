c4ea118eb0f9c2ce50f3e1846cb2472c79a2c289
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-20 22:54:46 +0000

    Feb 20, 2025, 11:54 PM

--- src/models/banner.model.ts
+++ src/models/banner.model.ts
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
-const BannerSchema = new mongoose.Schema({});
+const BannerSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["small", "big"],
+    required: true,
+    unique: true,
+  },
+});
 
 export default mongoose.models.Banner || mongoose.model("Banner", BannerSchema);
