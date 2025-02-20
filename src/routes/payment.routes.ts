57a2adba802d52335b95ba2440891e89df9de768
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-20 12:31:11 +0000

    Feb 20, 2025, 1:31 PM

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -9,5 +9,6 @@ const router = express.Router();
 
 router.post("/init", protect, initializePayment);
 router.get("/verify", verifyPayment);
+router.post("/webhook", verifyPayment);
 
 export default router;
