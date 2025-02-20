43fc9fa5770d3ca035c74e9f462a9ce3672daab1
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-20 02:38:16 +0000

    Feb 20, 2025, 3:38 AM

--- src/config/r2config.ts
+++ src/config/r2config.ts
@@ -2,18 +2,34 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 
 dotenv.config();
+const accessKeyId = process.env.R2_ACCESS_KEY_ID;
+const secretAccessKey = process.env.R2_SECRET_ACCESS_KEY;
+const endpoint = process.env.R2_ENDPOINT;
 
-const s3Client = new S3Client({});
+if (!accessKeyId || !secretAccessKey || !endpoint) {
+  throw new Error(
+    "Missing Cloudflare R2 credentials in environment variables."
+  );
+}
 
-// export const uploadToR2 = async (file) => {
-//   const fileName = `products/${Date.now()}-${file.originalname}`;
-//   const params = {
-//     Bucket: process.env.R2_BUCKET_NAME,
-//     Key: fileName,
-//     Body: file.buffer,
-//     ContentType: file.mimetype,
-//   };
+const s3Client = new S3Client({
+  region: "auto",
+  endpoint,
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+});
 
-//   await s3.send(new PutObjectCommand(params));
-//   return `${process.env.R2_ENDPOINT}/${process.env.R2_BUCKET_NAME}/${fileName}`;
-// };
+export const uploadToR2 = async (file: any) => {
+  const fileName = `products/${Date.now()}-${file.originalname}`;
+  const params = {
+    Bucket: process.env.R2_BUCKET_NAME,
+    Key: fileName,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+  };
+
+  await s3Client.send(new PutObjectCommand(params));
+  return `${process.env.R2_ENDPOINT}/${process.env.R2_BUCKET_NAME}/${fileName}`;
+};
