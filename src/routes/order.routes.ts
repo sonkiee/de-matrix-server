c34e61395caeb85171d5c553c4cf0f11dc7be441
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-20 04:16:48 +0000

    Feb 20, 2025, 5:16 AM

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,8 +1,20 @@
 import express from "express";
 import { protect } from "../middleware/auth.middleware";
-import { newOrder } from "../controllers/order.controller";
+import {
+  newOrder,
+  getOrderById,
+  getOrders,
+  getUserOrderById,
+  getUserOrders,
+} from "../controllers/order.controller";
+import { validateObjectId } from "../utils/mongodb-validation";
 
 const router = express.Router();
+
+router.get("/admin", getOrders);
+router.get("/admin/:id", getOrderById);
+router.get("/user/:id", protect, getUserOrders);
+router.get("/user/:id/:orderId", protect, getUserOrderById);
 
 router.post("/new", protect, newOrder);
 
