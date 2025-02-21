1b5a345a6650fcd4a3ce49cb2edf4e0473e2088e
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-21 01:55:40 +0000

    Feb 21, 2025, 2:55 AM

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,13 +3,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI!);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log("Error connecting to DB: ", error);
-    process.exit(1);
+const connectDB = async (attempts = 5, delay = 3000) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI!, {});
+      console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+      return;
+    } catch (error) {
+      console.error(
+        `❌ Database connection failed (Attempt ${i + 1}/${attempts})`
+      );
+      if (i < attempts - 1) {
+        console.log(`🔄 Retrying in ${delay / 1000} seconds...`);
+        await new Promise((res) => setTimeout(res, delay));
+        delay *= 2; // Exponential backoff
+      } else {
+        console.error("🚨 Could not connect to MongoDB. Exiting...");
+        process.exit(1);
+      }
+    }
   }
 };
 
