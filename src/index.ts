5865e8462574e807a644a5eb90724efd65fe4fd4
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-21 01:18:14 +0000

    Feb 21, 2025, 2:18 AM

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import hemlet from "helmet";
+import helmet from "helmet";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 import setupSwagger from "./swaggerConfig";
@@ -18,6 +18,9 @@ const app = express();
 setupSwagger(app);
 
 app.use(cors());
+app.use(helmet());
+
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
