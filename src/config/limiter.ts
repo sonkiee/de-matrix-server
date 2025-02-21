fb98fd26240e2af36a7e1b7af3604cf26b2ba20b
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-21 01:26:14 +0000

    Feb 21, 2025, 2:26 AM

--- src/config/limiter.ts
+++ src/config/limiter.ts
@@ -3,5 +3,5 @@ import rateLimit from "express-rate-limit";
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
-  message: "Too many requests, please try again later.",
+  message: "Too many requests from this IP, please try again later.",
 });
