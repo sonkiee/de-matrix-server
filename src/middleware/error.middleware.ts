a7bafef9b5fe67e02ca8919e768786a66abfe71a
sonkeydotcom <annagu.kennedy@gmail.com>
2025-02-21 01:49:23 +0000

    Feb 21, 2025, 2:49 AM

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -5,7 +5,9 @@ export const notFoundHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(404).json({ message: "Page not found" });
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
 };
 
 export const errorHandler = (
