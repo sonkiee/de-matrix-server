f445418491a2567c53acccd5d69c722a2dafebc7
sonkiee <annagu.kennedy@gmail.com>
2025-04-25 14:27:13 +0000

    Apr 25, 2025, 3:27 PM

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -22,10 +22,10 @@ export const errorHandler = (
   const statusCode = res.statusCode < 400 ? 500 : res.statusCode;
   res.status(statusCode).json({
     message: err.message || "Internal Server Error",
-    stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+    // stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
   });
 
-  if (process.env.NODE_ENV !== "production") {
-    console.error(err.stack);
-  }
+  // if (process.env.NODE_ENV !== "production") {
+  //   console.error(err.stack);
+  // }
 };
