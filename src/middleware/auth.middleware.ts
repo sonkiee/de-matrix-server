1094dd3c2efc4389f3086372986aa124c2463de4
sonkiee <annagu.kennedy@gmail.com>
2025-04-25 14:05:04 +0000

    Apr 25, 2025, 3:05 PM

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -21,10 +21,15 @@ export const protect = async (
 
   try {
     const decoded = verify(token) as { id: string };
-
-    req.user = await User.findById(decoded.id).select("-password");
-
     if (!req.user) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: Missing user context" });
+    }
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (user) {
       res.status(401).json({ message: "User not found" });
       return;
     }
