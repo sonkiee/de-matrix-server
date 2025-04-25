3dcc1831222cdce96051422d4947842e1f700f54
sonkiee <annagu.kennedy@gmail.com>
2025-04-25 15:03:32 +0000

    Apr 25, 2025, 4:03 PM

--- src/utils/http-only-cookies.ts
+++ src/utils/http-only-cookies.ts
@@ -5,6 +5,6 @@ export const set = (res: Response, token: string) => {
     httpOnly: true,
     secure: false,
     sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
   });
 };
