e441ef8d7189860ccbdf7f9eb1e4f32cb4abe00b
sonkiee <annagu.kennedy@gmail.com>
2025-05-07 11:41:53 +0000

    May 7, 2025, 12:41 PM

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,15 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS;
-
+console.log("Allowed origins:", ALLOWED_ORIGINS);
+if (!ALLOWED_ORIGINS) {
+  throw new Error("Missing ALLOWED_ORIGINS environment variable."); // Handle this error in your production code.
+}
 export const corsOptions = {
-  origin: ALLOWED_ORIGINS || "*",
+  origin: ALLOWED_ORIGINS, // or your frontend domain
   credentials: true, // ✅ allow cookies
   methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
   allowedHeaders: [
     "Content-Type",
     "Authorization",
-    ALLOWED_ORIGINS ? "Access-Control-Allow-Origin" : "",
+    "Access-Control-Allow-Origin",
   ],
 };
 
