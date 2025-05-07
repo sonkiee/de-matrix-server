82d4181a47f8fa9cb7ef057b4774cc34b3e2cf2a
sonkiee <annagu.kennedy@gmail.com>
2025-05-07 11:04:46 +0000

    May 7, 2025, 12:04 PM

--- src/utils/http-only-cookies.ts
+++ src/utils/http-only-cookies.ts
@@ -1,10 +1,16 @@
 import { Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const NODE_ENV = process.env.NODE_ENV;
 
 export const set = (res: Response, token: string) => {
+  const isProduction = NODE_ENV === "production";
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
   });
 };
